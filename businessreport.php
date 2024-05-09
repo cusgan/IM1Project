60321bb19e8004ef4c22b0c14c88625352a3f707
cusgan <fmfgbascug@gmail.com>
2024-05-09 10:15:13 +0800

    added statistics and charts

--- businessreport.php
+++ businessreport.php
@@ -16,6 +16,9 @@
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous"
         />
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
+        </script>
     </head>
 
     <body>        
@@ -36,24 +39,24 @@
                 <div class="col"><h2><?php echo $_SESSION['bname']; ?> Report</h2><br>
                     <ul class="nav nav-pills nav-fill">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page"  data-bs-toggle="pill" data-bs-target="#report1" href="#pills-home">Customer Demographics</a>
+                            <a class="nav-link active" aria-current="page"  data-bs-toggle="pill" data-bs-target="#report1" href="#pills-home">Tables</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link"  data-bs-toggle="pill" data-bs-target="#report2" href="#pills-profile">Item Report</a>
+                            <a class="nav-link"  data-bs-toggle="pill" data-bs-target="#report2" href="#pills-profile">Statistics</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" data-bs-toggle="pill" data-bs-target="#report3" href="#pills-contact">Order Quantity Report</a>
+                            <a class="nav-link" data-bs-toggle="pill" data-bs-target="#report3" href="#pills-contact">Charts</a>
                         </li>
                     </ul>
                     <div class="tab-content" id="pills-tabContent"><br>
                         <!-- first tab -->
                         <div class="tab-pane fade show active" id="report1" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
-                            <h3>All Items in Ascending Prices</h3>
+                            <h3>All Items by Price (Ascending)</h3>
                             <div
                                 class="table-responsive"
                             >
                                 <table
-                                    class="table table-primary"
+                                    class="table table-striped"
                                 >
                                     <thead>
                                         <tr>
@@ -66,7 +69,7 @@
                                     <tbody>
                                         <?php
                                             $item = 1;
-                                            $item_res = mysqli_query($connection,"SELECT * FROM tblmenuitem WHERE menuid='".$_SESSION['menuid']."' ORDER BY buyprice");
+                                            $item_res = mysqli_query($connection,"SELECT * FROM tblmenuitem WHERE menuid='".$_SESSION['menuid']."' ORDER BY buyprice ASC");
                                             while($item = mysqli_fetch_array($item_res)){
                                                 echo "<tr>
                                                         <td scope=\"row\">#$item[0]</td>
@@ -79,16 +82,14 @@
                                     </tbody>
                                 </table>
                             </div>
-                            
-                        </div>
-                        <!-- second tab -->
-                        <div class="tab-pane fade" id="report2" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">
+                            <br>
+
                             <h3>Items with 5 or more in stock</h3>
                             <div
                                 class="table-responsive"
                             >
                                 <table
-                                    class="table table-primary"
+                                    class="table table-striped"
                                 >
                                     <thead>
                                         <tr>
@@ -112,19 +113,179 @@
                                     </tbody>
                                 </table>
                             </div>
+                            <br>
+
+                            <h3>Total Assets Value</h3>
+                            <div
+                                class="table-responsive"
+                            >
+                                <table
+                                    class="table table-striped"
+                                >
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Total Assets</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                            $item = 1;
+                                            $item_res = mysqli_query($connection,"select SUM(qty*buyprice) from tblmenuitem where menuid='".$_SESSION['menuid']."'");
+                                            while($item = mysqli_fetch_array($item_res)){
+                                                echo "<td>₱$item[0]</td>";
+                                            }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+                        <!-- second tab -->
+                        <div class="tab-pane fade" id="report2" role="tabpanel" aria-labelledby="pills-profile-tab" tabindex="0">
+                            <h3>Statistics</h3>
+                            <div
+                                class="row justify-content-center align-items-center g-2"
+                            >
+                                <div class="col">
+                                    <div class="card" style="width: 18rem;">
+                                        <div class="card-body">
+                                            <h3 class="card-title">
+                                                <?php
+                                                    $item = 1;
+                                                    $item_res = mysqli_query($connection,"select COUNT(itemid) from tblmenuitem where menuid='".$_SESSION['menuid']."'");
+                                                    while($item = mysqli_fetch_array($item_res)){
+                                                        echo $item[0];
+                                                    }
+                                                ?>
+                                            </h3>
+                                            <h4 class="card-subtitle mb-2 text-body-secondary">Total No. of Menu Items</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card" style="width: 18rem;">
+                                        <div class="card-body">
+                                            <h3 class="card-title">₱
+                                                <?php
+                                                    $item = 1;
+                                                    $item_res = mysqli_query($connection,"select ROUND(SUM(buyprice),2) from tblmenuitem where menuid='".$_SESSION['menuid']."'");
+                                                    while($item = mysqli_fetch_array($item_res)){
+                                                        echo $item[0];
+                                                    }
+                                                ?>
+                                            </h3>
+                                            <h4 class="card-subtitle mb-2 text-body-secondary">Total Cost of Menu Items</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card" style="width: 18rem;">
+                                        <div class="card-body">
+                                            <h3 class="card-title">₱
+                                                <?php
+                                                    $item = 1;
+                                                    $item_res = mysqli_query($connection,"select ROUND(AVG(buyprice),2) from tblmenuitem where menuid='".$_SESSION['menuid']."'");
+                                                    while($item = mysqli_fetch_array($item_res)){
+                                                        echo $item[0];
+                                                    }
+                                                ?>
+                                            </h3>
+                                            <h4 class="card-subtitle mb-2 text-body-secondary">Average Menu Item Cost</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card" style="width: 18rem;">
+                                        <div class="card-body">
+                                            <h3 class="card-title">
+                                                <?php
+                                                    $item = 1;
+                                                    $item_res = mysqli_query($connection,"select ROUND(AVG(qty),2) from tblmenuitem where menuid='".$_SESSION['menuid']."'");
+                                                    while($item = mysqli_fetch_array($item_res)){
+                                                        echo $item[0];
+                                                    }
+                                                ?>
+                                            </h3>
+                                            <h4 class="card-subtitle mb-2 text-body-secondary">Average Quantity in Stock</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card" style="width: 18rem;">
+                                        <div class="card-body">
+                                            <h3 class="card-title">₱
+                                                <?php
+                                                    $item = 1;
+                                                    $item_res = mysqli_query($connection,"select ROUND(AVG(buyprice*qty),2) from tblmenuitem where menuid='".$_SESSION['menuid']."'");
+                                                    while($item = mysqli_fetch_array($item_res)){
+                                                        echo $item[0];
+                                                    }
+                                                ?>
+                                            </h3>
+                                            <h4 class="card-subtitle mb-2 text-body-secondary">Average Asset Value per Item</h4>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                            
 
                         </div>
                         <!-- third tab -->
                         <div class="tab-pane fade" id="report3" role="tabpanel" aria-labelledby="pills-contact-tab" tabindex="0">
-                            <h3>Total Assets Value</h3>
-                            <h5>
-                            ₱<?php
-                                            $item = 1;
-                                            $item_res = mysqli_query($connection,"select SUM(qty*buyprice) from tblmenuitem where menuid='".$_SESSION['menuid']."'");
-                                            while($item = mysqli_fetch_array($item_res)){
-                                                echo $item[0];
-                                            }
-                                        ?>.00 Pesos Dollars</h5>
+                            <h3>Charts</h3>
+                            <div
+                                class="row justify-content-center align-items-center g-2"
+                            >
+                                <div class="col">
+                                    <canvas id="myChart" style="width:100%;max-width:700px">
+                                        <script>
+                                            new Chart("myChart", {
+                                                type: "pie",
+                                                data: {
+                                                    labels: ['Pizza', 'Fries', 'Burger', 'Cake'],
+                                                    datasets: [{
+                                                    backgroundColor: ["#b91d47", "#00aba9", "#2b5797", "#e8c3b9"],
+                                                    data: [20, 15, 50, 10]
+                                                    }]
+                                                },
+                                                options: {
+                                                    title: {
+                                                    display: true,
+                                                    text: "Product Stock"
+                                                    }
+                                                }
+                                            });
+                                        </script>
+                                    </canvas>
+                                </div>
+
+                                <div class="col">
+                                    <canvas id="myChart2" style="width:100%;max-width:700px">
+                                        <script>
+                                            new Chart("myChart2", {
+                                                type: "line",
+                                                data: {
+                                                    labels: ['Pizza', 'Fries', 'Burger', 'Cake'],
+                                                    datasets: [{
+                                                    backgroundColor:"rgba(0,0,255,1.0)",
+                                                    borderColor: "rgba(0,0,255,0.1)",
+                                                    data: [20, 15, 30, 50],
+                                                    fill: false
+                                                    }]
+                                                },
+                                                options:{
+                                                    title: {
+                                                    legend: {display: false},
+                                                    display: true,
+                                                    text: "Item Price",
+                                                    }}
+                                            });
+                                        </script>
+                                    </canvas>
+                                </div>
+                            </div>
 
                         </div>
                     </div>
